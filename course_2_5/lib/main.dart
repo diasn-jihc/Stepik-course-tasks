249e0303330c1c51e669ff89a890257081ba1582
diasn-jihc <nurlan.yltar@gmail.com>
2026-09-09 12:53:22 +0500

    2.5 кайталадым

---
+++ course_2_5/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text(
+          "Hello Flutter",
+          style: TextStyle(fontSize: 30),
+          textDirection: TextDirection.ltr,
+        ),
+      ),
+    ),
+  );
+}
