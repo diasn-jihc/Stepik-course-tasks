5fb5920d0826091b3c41af06458d3284161271c7
diasn-jihc <nurlan.yltar@gmail.com>
2026-09-14 08:26:32 +0500

    3.6 oryndadym

---
+++ course_3_6/android/app/src/main/kotlin/com/example/course_3_6/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.course_3_6
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ course_3_6/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Building layouts"), centerTitle: true),
+        body: Row(
+          children: <Widget>[
+            Expanded(
+              flex: 2,
+              child: Image.network(
+                'https://glimmernet.com/wp-content/uploads/2019/02/clip-path-example-bg.jpg',
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Container(
+                padding: EdgeInsets.all(30),
+                color: Colors.red,
+                child: Text('1'),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                padding: EdgeInsets.all(30),
+                color: Colors.green,
+                child: Text('2'),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: EdgeInsets.all(30),
+                color: Colors.blue,
+                child: Text('3'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
