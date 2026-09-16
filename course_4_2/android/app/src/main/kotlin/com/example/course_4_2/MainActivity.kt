abf867d95261714897143f3b1ba284e6ba33e122
diasn-jihc <nurlan.yltar@gmail.com>
2026-09-16 12:36:12 +0500

    oryndadym

---
+++ course_4_2/android/app/src/main/kotlin/com/example/course_4_2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.course_4_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ course_4_2/lib/main.dart
@@ -0,0 +1,15 @@
+// Sound Null Safety
+
+// Dart - типобезопасный (type-safe) язык.
+
+// В основе Null Safety лежат 3 принципа:
+// 1. Non-nullable by default
+// 2. Fully Sound
+// 3. Incrementally adoptable
+
+void main() {
+  int? age;
+  String name = 'Mark';
+  print(name);
+  print(age);
+}
