b98432dfbcb161d39abb6cd6c6d44d735d920332
diasn-jihc <nurlan.yltar@gmail.com>
2026-09-16 15:16:54 +0500

    oryndadym

---
+++ course_4_6/bin/cascade.dart
@@ -0,0 +1,32 @@
+// Sound Null Safety
+
+// ?.. - cascade null-aware operator
+
+void main() {
+  Path? path;
+
+  path
+    ?..moveTo(0, 0)
+    ..lineTo(0, 2)
+    ..lineTo(2, 2);
+}
+
+class Path {
+  void moveTo(int x, int y) {}
+  void lineTo(int x, int y) {}
+}
+
+
+
+//void main() {
+//  Path path = Path();
+
+//  path.moveTo(0, 0)
+//  path.lineTo(0, 2)
+//  path.lineTo(2, 2);
+//}
+
+//class Path {
+//  void moveTo(int x, int y) {}
+//  void lineTo(int x, int y) {}
+//}
