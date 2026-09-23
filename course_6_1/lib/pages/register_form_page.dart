bf735186f3c0f1807e1388bda97e556fbbf8b241
diasn-jihc <nurlan.yltar@gmail.com>
2026-09-23 13:33:18 +0500

    oryndadym

---
+++ course_6_1/lib/pages/register_form_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class RegisterFormPage extends StatefulWidget {
+  const RegisterFormPage({super.key});
+
+  @override
+  State<RegisterFormPage> createState() => _RegisterFormPageState();
+}
+
+class _RegisterFormPageState extends State<RegisterFormPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Register Form'),
+        centerTitle: true,
+      ), // AppBar
+      body: Form(
+        child: ListView(
+          padding: EdgeInsets.all(16.0),
+          children: [],
+        ), // ListView
+      ), // Form
+    ); // Scaffold
+  }
+}
