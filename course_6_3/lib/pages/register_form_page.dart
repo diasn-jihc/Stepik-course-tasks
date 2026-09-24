fe7d8ac45f75decbe07343c666f613e110b0cfd5
diasn-jihc <nurlan.yltar@gmail.com>
2026-09-24 11:26:14 +0500

    oryndadym

---
+++ course_6_3/lib/pages/register_form_page.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+
+class RegisterFormPage extends StatefulWidget {
+  const RegisterFormPage({super.key});
+
+  @override
+  State<RegisterFormPage> createState() => _RegisterFormPageState();
+}
+
+class _RegisterFormPageState extends State<RegisterFormPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Register Form'),
+        centerTitle: true,
+      ),
+      body: Form(
+        child: ListView(
+          padding: EdgeInsets.all(16.0),
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Full Name *',
+                hintText: 'What do people call you?',
+                prefixIcon: Icon(Icons.person),
+                suffixIcon: Icon(
+                  Icons.delete_outline,
+                  color: Colors.red,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  borderSide: BorderSide(color: Colors.black, width: 2.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Phone Number *',
+                hintText: 'Where can we reach you?',
+                helperText: 'Phone format: (XXX)XXX-XXXX',
+                prefixIcon: Icon(Icons.call),
+                suffixIcon: Icon(
+                  Icons.delete_outline,
+                  color: Colors.red,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  borderSide: BorderSide(color: Colors.black, width: 2.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Email Address',
+                hintText: 'Enater a email address',
+                icon: Icon(Icons.mail),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Life Story',
+                hintText: 'Tell us about your self',
+                helperText: 'Keep it short, this is just a demo',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Password *',
+                hintText: 'Enter the password',
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.visibility),
+                  onPressed: () {},
+                ),
+                icon: Icon(Icons.security),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Confirm Password *',
+                hintText: 'Confirm the password',
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.visibility),
+                  onPressed: () {},
+                ),
+                icon: Icon(Icons.border_color),
+              ),
+            ),
+            SizedBox(height: 15),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+              ),
+              child: Text(
+                'Submit Form',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
