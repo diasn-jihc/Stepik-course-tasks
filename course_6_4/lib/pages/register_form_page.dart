f668e22a79ae62458d4150a961846d3ebc358e10
diasn-jihc <nurlan.yltar@gmail.com>
2026-09-24 11:54:15 +0500

    oryndadym

---
+++ course_6_4/lib/pages/register_form_page.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class RegisterFormPage extends StatefulWidget {
+  const RegisterFormPage({super.key});
+
+  @override
+  State<RegisterFormPage> createState() => _RegisterFormPageState();
+}
+
+class _RegisterFormPageState extends State<RegisterFormPage> {
+  bool _hidePass = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Register Form'),
+        centerTitle: true,
+      ),
+      body: Form(
+        child: ListView(
+          padding: EdgeInsets.all(16.0),
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Full Name *',
+                hintText: 'What do people call you?',
+                prefixIcon: Icon(Icons.person),
+                suffixIcon: Icon(
+                  Icons.delete_outline,
+                  color: Colors.red,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  borderSide: BorderSide(color: Colors.black, width: 2.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              keyboardType: TextInputType.phone,
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+              ],
+              decoration: InputDecoration(
+                labelText: 'Phone Number *',
+                hintText: 'Where can we reach you?',
+                helperText: 'Phone format: (XXX)XXX-XXXX',
+                prefixIcon: Icon(Icons.call),
+                suffixIcon: Icon(
+                  Icons.delete_outline,
+                  color: Colors.red,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  borderSide: BorderSide(color: Colors.black, width: 2.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: 'Email Address',
+                hintText: 'Enater a email address',
+                icon: Icon(Icons.mail),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Life Story',
+                hintText: 'Tell us about your self',
+                helperText: 'Keep it short, this is just a demo',
+                border: OutlineInputBorder(),
+              ),
+              maxLines: 3,
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              obscureText: _hidePass,
+              maxLength: 8,
+              decoration: InputDecoration(
+                labelText: 'Password *',
+                hintText: 'Enter the password',
+                suffixIcon: IconButton(
+                  icon: Icon(_hidePass ? Icons.visibility : Icons.visibility_off),
+                  onPressed: () {
+                    setState(() {
+                      _hidePass = !_hidePass;
+                    });
+                  },
+                ),
+                icon: Icon(Icons.security),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              obscureText: _hidePass,
+              maxLength: 8,
+              decoration: InputDecoration(
+                labelText: 'Confirm Password *',
+                hintText: 'Confirm the password',
+                icon: Icon(Icons.border_color),
+              ),
+            ),
+            SizedBox(height: 15),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+              ),
+              child: Text(
+                'Submit Form',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
